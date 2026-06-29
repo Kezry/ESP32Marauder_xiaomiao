e9756c25a9f4f911c0773e383c20228f3a45051f
Kezry <c.jac@foxmail.com>
2026-06-29 17:18:26 +0800

    Remove HAS_SEPARATE_SD from XiaoMiao DISPLAY block too

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -2091,7 +2091,7 @@
 
       // SD card shares SPI bus with TFT (SPI2: SCK=18, MOSI=23, MISO=19),
       // uses chip select (CS) for time-division multiplexing.
-      #define HAS_SEPARATE_SD
+
       #define SD_SCK   TFT_SCLK
       #define SD_MISO  TFT_MISO
       #define SD_MOSI  TFT_MOSI
